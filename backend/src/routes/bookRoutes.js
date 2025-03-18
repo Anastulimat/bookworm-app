0134ad24f420a3fcd4adf3181ea0a62583440b99
Anas TULIMAT <anas.tulimat@orange.com>
2025-03-18 18:49:52 +0100

    feat: finish the backend

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -43,5 +43,86 @@ router.post("/", protectRoute, async (req, res) => {
     }
 });
 
+router.get("/", protectRoute, async (req, res) => {
+    try {
+        const page = req.query.page || 1;
+        const limit = req.query.limit || 5;
+        const skip = (page - 1) * limit;
+
+
+        const books = await Book.find()
+            .sort({createdAt: -1})
+            .skip(skip)
+            .limit(limit)
+            .populate("user", "username profileImage");
+
+        const totalBooks = await Book.countDocuments();
+
+        res.send({
+            books,
+            currentPage: page,
+            totalBooks,
+            totalPages: Math.ceil(totalBooks / limit),
+        });
+
+    } catch (error) {
+        console.log("Error in get all books route", error);
+        res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+});
+
+router.delete("/:id", protectRoute, async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        if (!book) {
+            res.status(404).json({
+                message: "Book not found"
+            });
+        }
+
+        if (book.user.toString() !== req.user._id.toString()) {
+            res.status(401).json({
+                message: "You are not authorized"
+            });
+        }
+
+        // Delete image from cloudinary
+        if (book.image && book.image.includes("cloudinary")) {
+            try {
+                const publicId = book.image.split("/").pop().split(".")[0];
+                await cloudinary.uploader.destroy(publicId);
+            } catch (deleteError) {
+                console.log("Error deleting image from cloudinary");
+            }
+        }
+
+        await book.deleteOne();
+
+        res.status(200).json({
+            message: "Book deleted successfully"
+        });
+
+    } catch (error) {
+        console.log("Error deleting book", error);
+        res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
+});
+
+router.get("/user", protectRoute, async (req, res) => {
+    try {
+        const books = await Book.find({user: req.user._id}).sort({createdAt: -1});
+        res.status(200).json(books);
+    } catch (error) {
+        console.log("Get user books error", error);
+        res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+})
+
 
 export default router;
