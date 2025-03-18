7263d538b1c56020878d26040f4e7ac84dd2507e
Anas TULIMAT <anas.touliemat@gmail.com>
2025-03-18 23:08:01 +0100

    fix: add KeyboardAvoidingView to fix keyboard covering fields

--- mobile/app/(auth)/index.jsx
+++ mobile/app/(auth)/index.jsx
@@ -1,4 +1,13 @@
-import {View, Text, Image, TextInput, TouchableOpacity, ActivityIndicator} from 'react-native';
+import {
+    View,
+    Text,
+    Image,
+    TextInput,
+    TouchableOpacity,
+    ActivityIndicator,
+    KeyboardAvoidingView,
+    Platform
+} from 'react-native';
 
 import styles from '../../assets/styles/login.styles';
 import {useState} from "react";
@@ -19,101 +28,106 @@ const Login = () => {
     }
 
     return (
-        <View style={styles.container}>
-            {/* Illustration */}
-            <View style={styles.topIllustration}>
-                <Image
-                    source={require('../../assets/images/reading-glasses-bro.png')}
-                    style={styles.illustrationImage}
-                    resizeMode="contain"
-                />
-            </View>
+        <KeyboardAvoidingView
+            style={{flex: 1}}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        >
+            <View style={styles.container}>
+                {/* Illustration */}
+                <View style={styles.topIllustration}>
+                    <Image
+                        source={require('../../assets/images/reading-glasses-bro.png')}
+                        style={styles.illustrationImage}
+                        resizeMode="contain"
+                    />
+                </View>
 
-            <View style={styles.card}>
-                <View style={styles.formContainer}>
-                    {/* Email */}
-                    <View style={styles.inputGroup}>
-                        <Text style={styles.label}>Email</Text>
-                        <View style={styles.inputContainer}>
-                            <Ionicons
-                                name="mail-outline"
-                                size={20}
-                                color={COLORS.primary}
-                                style={styles.inputIcon}
-                            />
-                            <TextInput
-                                style={styles.input}
-                                placeholder="Enter your email"
-                                placeholderTextColor={COLORS.placeholderText}
-                                value={email}
-                                onChangeText={setEmail}
-                                keyboardType="email-address"
-                                autoCapitalize="none"
-                            />
-                        </View>
-                    </View>
-
-                    {/* Password */}
-                    <View style={styles.inputGroup}>
-                        <Text style={styles.label}>Password</Text>
-                        <View style={styles.inputContainer}>
-                            {/* Left icon */}
-                            <Ionicons
-                                name="lock-closed-outline"
-                                size={20}
-                                color={COLORS.primary}
-                                style={styles.inputIcon}
-                            />
-                            {/* Input */}
-                            <TextInput
-                                style={styles.input}
-                                placeholder="Enter your passowrd"
-                                placeholderTextColor={COLORS.placeholderText}
-                                value={password}
-                                onChangeText={setPassword}
-                                secureTextEntry={!showPassword}
-                            />
-
-                            <TouchableOpacity
-                                onPress={() => setShowPassword(!showPassword)}
-                                style={styles.eyeIcon}
-                            >
+                <View style={styles.card}>
+                    <View style={styles.formContainer}>
+                        {/* Email */}
+                        <View style={styles.inputGroup}>
+                            <Text style={styles.label}>Email</Text>
+                            <View style={styles.inputContainer}>
                                 <Ionicons
-                                    name={showPassword ? "eye-outline" : "eye-off-outline"}
+                                    name="mail-outline"
                                     size={20}
                                     color={COLORS.primary}
+                                    style={styles.inputIcon}
                                 />
-                            </TouchableOpacity>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="Enter your email"
+                                    placeholderTextColor={COLORS.placeholderText}
+                                    value={email}
+                                    onChangeText={setEmail}
+                                    keyboardType="email-address"
+                                    autoCapitalize="none"
+                                />
+                            </View>
                         </View>
+
+                        {/* Password */}
+                        <View style={styles.inputGroup}>
+                            <Text style={styles.label}>Password</Text>
+                            <View style={styles.inputContainer}>
+                                {/* Left icon */}
+                                <Ionicons
+                                    name="lock-closed-outline"
+                                    size={20}
+                                    color={COLORS.primary}
+                                    style={styles.inputIcon}
+                                />
+                                {/* Input */}
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="Enter your passowrd"
+                                    placeholderTextColor={COLORS.placeholderText}
+                                    value={password}
+                                    onChangeText={setPassword}
+                                    secureTextEntry={!showPassword}
+                                />
+
+                                <TouchableOpacity
+                                    onPress={() => setShowPassword(!showPassword)}
+                                    style={styles.eyeIcon}
+                                >
+                                    <Ionicons
+                                        name={showPassword ? "eye-outline" : "eye-off-outline"}
+                                        size={20}
+                                        color={COLORS.primary}
+                                    />
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={handleLogin}
+                            disabled={isLoading}
+                        >
+                            {isLoading ? (
+                                <ActivityIndicator color="#fff"/>
+                            ) : (
+                                <Text style={styles.buttonText}>Login</Text>
+                            )}
+                        </TouchableOpacity>
+
+                        {/* Footer */}
+                        <View style={styles.footer}>
+                            <Text style={styles.footerText}>
+                                Don't have an account?
+                            </Text>
+                            <Link href="/signup" asChild>
+                                <TouchableOpacity>
+                                    <Text style={styles.link}>Sign Up</Text>
+                                </TouchableOpacity>
+                            </Link>
+                        </View>
+
                     </View>
-
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={handleLogin}
-                        disabled={isLoading}
-                    >
-                        {isLoading ? (
-                            <ActivityIndicator color="#fff"/>
-                        ) : (
-                            <Text style={styles.buttonText}>Login</Text>
-                        )}
-                    </TouchableOpacity>
-
-                    {/* Footer */}
-                    <View style={styles.footer}>
-                        <Text style={styles.footerText}>
-                            Don't have an account?
-                        </Text>
-                        <Link href="/signup" asChild>
-                            <TouchableOpacity>
-                                <Text style={styles.link}>Sign Up</Text>
-                            </TouchableOpacity>
-                        </Link>
-                    </View>
-
                 </View>
             </View>
-        </View>
+        </KeyboardAvoidingView>
     );
 };
 
