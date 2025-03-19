a341f8d4902aa66be8362220f2dae2c27aeaed39
Anas TULIMAT <anas.touliemat@gmail.com>
2025-03-19 23:27:02 +0100

    feat: add tabs navigation

---
+++ mobile/app/(tabs)/profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const Profile = () => {
+    return (
+        <View>
+            <Text>Profile</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default Profile;
